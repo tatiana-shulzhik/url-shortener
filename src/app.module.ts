8cae0523e1f79953d1a6a735741cab1caccc08b5
Татьяна <tatiana.shulzhik@gmail.com>
2025-01-16 21:45:36 +0300

    fix: переименована директория 'user' в 'auth'

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
-import { AuthModule } from './user/auth.module';
+import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { SessionModule } from './session/session.module';
 
--- src/session/entities/session.entity.ts
+++ src/session/entities/session.entity.ts
@@ -1,4 +1,4 @@
-import { User } from '../../user/entities/user.entity';
+import { User } from '../../auth/entities/user.entity';
 import { Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
 
 @Entity()
